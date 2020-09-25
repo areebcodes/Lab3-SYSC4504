254d0a68679a64d950d2e45c4a2e6cd3261bbf92
Areeb <areebulhaq@cmail.carleton.ca>
2020-09-25 09:56:13 -0400

    Added a print statement

---
+++ src/AddressBook.java
@@ -0,0 +1,22 @@
+import java.util.*;
+public class AddressBook {
+
+
+    public static LinkedList<BuddyInfo> budList = new LinkedList();
+
+    public void addBuddy(BuddyInfo y) {
+
+        budList.add(bud);
+
+    }
+
+    public void removeBuddy(BuddyInfo x)
+    {
+        budList.remove(x);
+    }
+
+    public static void main(String[] name) {
+        System.out.println("Address Book");
+    }
+
+}
